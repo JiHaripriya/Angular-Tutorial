7b2f7e1942cf0bd00eded60e611f1f146886f628
Haripriya <haripriya@qburst.com>
2020-12-07 11:48:38 +0530

    Log method for recording user status change

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -8,6 +8,8 @@ export class UsersService {
 
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
+  toActiveCount = 0;
+  toInactiveCount = 0;
 
   userSetToInactive = new EventEmitter<number>();
   userSetToActive = new EventEmitter<number>();
@@ -17,10 +19,22 @@ export class UsersService {
   onSetToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.toInactiveLog();
   }
 
   onSetToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.toActiveLog();
+  }
+
+  toActiveLog() {
+    this.toActiveCount++;
+    console.log('Number of times inactive was set to active: ', this.toActiveCount);
+  }
+
+  toInactiveLog() {
+    this.toInactiveCount++;
+    console.log('Number of times active was set to inactive: ', this.toInactiveCount);
   }
 }
